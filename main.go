4b7289a0a972f55c69547885d22d496254e4c83d
fk-pixel <fthbtlkrt@gmail.com>
2022-09-17 17:10:28 +0200

    added remaining tickets rule

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -32,9 +32,11 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainigTickets = remainigTickets - userTickets
+
 	//userName = "John"
 	//userTickets = 2
 	//fmt.Printf("User %v-%v booked %v tickets.\n", firstName, lastName, userTickets) 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	
+	fmt.Printf("%v tickets remaining for %v\n", remainigTickets, conferenceName)	
 }
